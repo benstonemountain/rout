293e4f41fcaf2ebcd5dd4df12bf099735e8a35f3
StoneMountain <kohegyibence@yahoo.com>
2025-07-31 21:45:22 +0200

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { FromComponent } from './components/from/from.component';
 import { ToComponent } from './components/to/to.component';
-import { routeGuard } from './guard/route.guard';
+import {  canComponentDeactivateGuard } from './guard/route.guard';
 
 export const routes: Routes = [
-  { path: 'from', component: FromComponent, canDeactivate: [routeGuard] },
+  { path: 'from', component: FromComponent, canDeactivate: [canComponentDeactivateGuard] },
   { path: '', redirectTo: 'from', pathMatch: 'full' },
 
   { path: 'to', component: ToComponent },
